0165f1c7effc950ea5e0b4f2794cceccf62c7306
Matthew Fan <fn.matthew@outlook.com>
2017-10-24 17:54:17 -0500

    Initial

---
+++ Data Structures/DataStructure.h
@@ -0,0 +1,24 @@
+/**
+ * Programmer:		Matthew Fan
+ * Date Created:	10/24/17
+ */
+#ifndef DATA_STRUCTURE_H
+#define DATA_STRUCTURE_H
+
+
+#include <iostream>
+
+
+/// Abstract class for data structures
+template <typename T>
+class DataStructure
+{
+	public:
+
+
+	private:
+
+};
+
+
+#endif
